93c5f0c7f607336406871bf4fb7d8b536d7beb3c
akhremYuri <akhremr@gmail.com>
2024-02-21 20:44:08 +0300

    first variant of the game

--- jumpingGame.js
+++ jumpingGame.js
@@ -9,76 +9,96 @@ if (!canvas.getContext) {
 
 const ctx = canvas.getContext("2d");
 
-const cLandLevelOnY = 88; //px
-const cJumpLevelOnY = 18; //px;
+const cLandLevelOnY_v1 = 138; //px
+const cJumpLevelOnY = 80; //px;
+const PLAYER_X_POSITION = 50,
+  PLAYER_X_DELTA = 10,
+  PLAYER_Y_HANDS_LEVEL = 20;
 
-function drawThePlayer(y = cLandLevelOnY) {
+function getPLAYER_END_POINTS(y) {
+  return [
+    new Point(PLAYER_X_POSITION - PLAYER_X_DELTA, y),
+    new Point(PLAYER_X_POSITION + PLAYER_X_DELTA, y),
+    new Point(PLAYER_X_POSITION - PLAYER_X_DELTA, y - PLAYER_Y_HANDS_LEVEL),
+    new Point(PLAYER_X_POSITION + PLAYER_X_DELTA, y - PLAYER_Y_HANDS_LEVEL),
+  ];
+}
+
+function drawThePlayer(x = PLAYER_X_POSITION, y = cLandLevelOnY_v1) {
   ctx.beginPath();
-  ctx.moveTo(60, y);
-  ctx.arc(50, y, 10, 0, Math.PI * 2, true);
-  ctx.moveTo(50, y + 10);
-  ctx.lineTo(50, y + 30);
-  ctx.lineTo(40, y + 40);
-  ctx.moveTo(50, y + 30);
-  ctx.lineTo(60, y + 40);
-  ctx.moveTo(40, y + 22);
-  ctx.lineTo(60, y + 22);
+  ctx.arc(x, y - 40, 10, 0, Math.PI * 2, true);
+  ctx.moveTo(x, y - 10);
+  ctx.lineTo(x - PLAYER_X_DELTA, y);
+  ctx.moveTo(x, y - 10);
+  ctx.lineTo(x + 10, y);
+  ctx.moveTo(x, y - 10);
+  ctx.lineTo(x, y - 30);
+  ctx.moveTo(x - PLAYER_X_DELTA, y - PLAYER_Y_HANDS_LEVEL);
+  ctx.lineTo(x + PLAYER_X_DELTA, y - PLAYER_Y_HANDS_LEVEL);
   ctx.stroke();
 }
 
 function clearPlayerPartOfCanvas(y) {
   const cWidthOfPlayer = 22;
   const cHeightOfPlayer = 52;
-  const x0 = 39; //starting point of Player;
-  ctx.clearRect(x0, y - 11, cWidthOfPlayer, cHeightOfPlayer);
+  const x0 = PLAYER_X_POSITION - PLAYER_X_DELTA - 1; //starting point of Player;
+  ctx.clearRect(x0, y - 51, cWidthOfPlayer, cHeightOfPlayer);
 }
 
-let jumpLevel = cLandLevelOnY;
-const cPlayerStep = 5;
+let jumpLevel = cLandLevelOnY_v1;
+const cPlayerStep = 3;
 let gameStopped = true;
+let keyUpTimerId;
+let score;
+const speedOfGame = 15;
 
 function movePlayer(step = 1) {
   clearPlayerPartOfCanvas(jumpLevel);
   jumpLevel += step;
-  drawThePlayer(jumpLevel);
+  drawThePlayer(PLAYER_X_POSITION, jumpLevel);
 }
 
 function keyDownEvent(event) {
   if (event.code == "Space") {
-    if (gameStopped) {
-      runGame();
-    } else {
-      if (jumpLevel > cJumpLevelOnY) {
-        movePlayer(-cPlayerStep);
-      }
+    if (gameStopped) return;
+
+    if (jumpLevel > cJumpLevelOnY) {
+      movePlayer(-cPlayerStep);
     }
   } else if (event.code == "Enter" && gameStopped) {
     startGame();
   } else if (event.code == "Escape" && !gameStopped) {
     gamePause();
   }
-  console.log(event.code);
-  console.log(`keyDown event (gameStopped = ${gameStopped})`);
+  // console.log(event.code);
+  // console.log(`keyDown event (gameStopped = ${gameStopped})`);
 }
 
-let keyUpTimerId;
-const speedOfGame = 15;
+function clearKeyUpTimer() {
+  if (keyUpTimerId) clearTimeout(keyUpTimerId);
+  keyUpTimerId = null;
+}
 
 function movePlayerDown() {
+  if (gameStopped) {
+    clearKeyUpTimer();
+    return;
+  }
   movePlayer(cPlayerStep);
-  console.log("movePlayer Down");
-  if (jumpLevel < cLandLevelOnY) {
+  // console.log("movePlayer Down");
+  if (jumpLevel < cLandLevelOnY_v1) {
     keyUpTimerId = setTimeout(movePlayerDown, 1);
   } else {
-    console.log("movePlayerDown: clearTimeout");
-    clearTimeout(keyUpTimerId);
+    // console.log("movePlayerDown: clearTimeout");
+    clearKeyUpTimer();
   }
 }
 
 function keyUpEvent(event) {
+  if (gameStopped) return;
   if (event.code == "Space") {
     keyUpTimerId = setTimeout(movePlayerDown, 1);
-    console.log("keyUp event");
+    // console.log("keyUp event");
   }
 }
 
@@ -87,6 +107,7 @@ document.addEventListener("keyup", keyUpEvent);
 
 const cRoadY = 140;
 let animateX = cWidthOfCanvas;
+let gameDuration = 0;
 
 function drawRoad() {
   ctx.beginPath();
@@ -96,6 +117,7 @@ function drawRoad() {
 }
 
 const cRadiusT = 15;
+const cTrank = 10;
 let treesArr = [0, 200, 400, 600];
 let treesType = treesArr.map(() => getRandomInt(2));
 
@@ -103,17 +125,45 @@ function isInCanvasArea(x) {
   return x < -(cRadiusT * 2) || x > cWidthOfCanvas + cRadiusT * 2;
 }
 
-function drawTrees(x0) {
-  function drawTree(x, y, radius = cRadiusT) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x, y - 30);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(x, y - 30, radius, (Math.PI / 180) * 135, (Math.PI / 180) * 45);
-    ctx.fill();
+function drawTree(x, y, radius = cRadiusT, isBigTree = false) {
+  let k = 1;
+
+  if (isBigTree) k = 1.3;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y - cTrank * k - radius * k);
+  ctx.stroke();
+  const startAngleT = (Math.PI / 180) * 135;
+  const endAngleT = (Math.PI / 180) * 45;
+  let y1 = y - cTrank * k - radius * Math.sin(startAngleT);
+  ctx.moveTo(x, y1);
+  radius *= k;
+  ctx.arc(x, y1, radius, startAngleT, endAngleT);
+  ctx.fill();
+
+  function isCollision(arrOfPoints) {
+    for (let i = 0; i < arrOfPoints.length; i++) {
+      if (ctx.isPointInPath(arrOfPoints[i].x, arrOfPoints[i].y)) return true;
+    }
+    return false;
   }
 
+  let treeXstart = x - radius,
+    treeXend = x + radius;
+  if (
+    (PLAYER_X_POSITION - PLAYER_X_DELTA <= treeXstart &&
+      PLAYER_X_POSITION + PLAYER_X_DELTA >= treeXstart) ||
+    (PLAYER_X_POSITION - PLAYER_X_DELTA <= treeXend &&
+      PLAYER_X_POSITION + PLAYER_X_DELTA >= treeXend)
+  ) {
+    if (isCollision(getPLAYER_END_POINTS(jumpLevel))) gameOver();
+  }
+  if (treeXend === PLAYER_X_POSITION - PLAYER_X_DELTA - 1) score += 10;
+  ctx.clearRect(500, 30, 100, 40);
+  writeTextToCanvas(`Score: ${score}`, 14, 500, 40);
+}
+
+function drawTrees(x0) {
   for (let i = 0; i < treesArr.length; i++) {
     let addTree = treesType[i];
     drawTree(x0 + treesArr[i], cRoadY);
@@ -125,7 +175,12 @@ function drawTrees(x0) {
 
 function clearTrees(x0) {
   function clearTree(x, y, radius) {
-    ctx.clearRect(x - radius, y - 45, radius * 2, 30 + radius);
+    ctx.clearRect(
+      x - radius,
+      y - cTrank - radius * 2,
+      radius * 2,
+      cTrank + radius * 2
+    );
   }
 
   let y = cRoadY - 1;
@@ -140,12 +195,17 @@ function clearTrees(x0) {
 }
 
 function animateTrees() {
+  ctx.clearRect(500, 5, 100, 40);
+  let timeVal = gameDuration / 1000;
+  writeTextToCanvas(`time: ${timeVal.toFixed(3)} s`, 14, 500, 20);
+  gameDuration += speedOfGame;
   clearTrees(animateX);
   animateX--;
   drawTrees(animateX);
   if (animateX < -(cWidthOfCanvas + cRadiusT * 2 + 30))
     animateX = cWidthOfCanvas;
-  console.log(animateX);
+
+  // console.log(animateX);
 }
 
 function draw() {
@@ -153,8 +213,8 @@ function draw() {
   drawThePlayer();
 }
 
-function writeTextToCanvas(text, x = 100, y = 30) {
-  ctx.font = "28px serif";
+function writeTextToCanvas(text, fontSize = 28, x = 100, y = 30) {
+  ctx.font = `${fontSize}px serif`;
   ctx.fillText(text, x, y);
 }
 
@@ -171,7 +231,7 @@ function startInterval() {
 
 function stopInterval() {
   clearInterval(timerId);
-  console.log("Interval stopped.");
+  // console.log("Interval stopped.");
 }
 
 function clearAllCanvas() {
@@ -180,10 +240,13 @@ function clearAllCanvas() {
 
 function startGame() {
   animateX = cWidthOfCanvas;
+  jumpLevel = cLandLevelOnY_v1;
+  score = 0;
+  gameDuration = 0;
   clearAllCanvas();
   draw();
   runGame();
-  console.log("startGame");
+  // console.log("startGame");
 }
 
 function clearTextInfoOnCanvas() {
@@ -196,28 +259,19 @@ function runGame() {
   clearTextInfoOnCanvas();
 }
 
-function gamePause() {
-  stopInterval();
-  gameStopped = true;
-  writeTextToCanvas("Game on pause.");
-  writeTextToCanvas("Press space key to continue.", 100, 70);
-  console.log("gamePause");
-}
-
 function gameOver() {
   stopInterval();
   gameStopped = true;
+  drawThePlayer(PLAYER_X_POSITION, jumpLevel); //delete!
   writeTextToCanvas("Game over.");
-  writeTextToCanvas("Press any key to start again.", 100, 70);
+  writeTextToCanvas("Press 'Enter' key to start again.", 21, 100, 70);
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-// window.main = () => {
-//   window.requestAnimationFrame(main);
-//   animateTrees();
-//   console.log(`animateX: ${animateX}`);
-// };
-// main();
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
